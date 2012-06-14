af5961c93ccd0bdace5df646d6b10eb3055d1f3e
JustTB <justusin@gmx.de>
2012-06-14 19:41:45 +0200

    last commit today

--- src/org/gpsanonymity/Main.java
+++ src/org/gpsanonymity/Main.java
@@ -121,6 +121,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		initialize();
+		downloadData();
 		simulateTrackGridMerge();
 		simulateTrackKMeansMerge();
 		simulateTrackSegmentCloudMerge();
